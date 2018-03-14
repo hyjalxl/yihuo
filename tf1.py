f36f1275a40f3e9c0870bd3f487d3d659cc93c24
yihuo <306095363@qq.com>
2018-03-14 10:47:38 +0800

    nothing.

---
+++ tf1.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+# user=hu
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+
+tf.logging.set_verbosity(tf._logging_ops)
+
+if __name__ == '__main__':
+    tf.app.run()
+    pass
