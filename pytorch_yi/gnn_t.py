c8305df94904bb554467700272d54b35f74d1841
yihuo <306095363@qq.com>
2018-03-24 11:59:19 +0800

    helps

---
+++ pytorch_yi/gnn_t.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+# user=hu
+
+"""
+test pytorch_gnn
+"""
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+if __name__ == '__main__':
+    pass
